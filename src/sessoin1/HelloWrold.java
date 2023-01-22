aa688d83d53b6d1fbdc41b14b22814b251eead39
TairovichJR <yokubov.omonjon@gmail.com>
2023-01-21 20:05:35 -0500

    added another print statement

--- src/sessoin1/HelloWrold.java
+++ src/sessoin1/HelloWrold.java
@@ -5,5 +5,6 @@ public class HelloWrold {
     public static void main(String[] args) {
 
         System.out.println("Hello Batch 7!!!!");
+        System.out.println("This is another line of print");
     }
 }
