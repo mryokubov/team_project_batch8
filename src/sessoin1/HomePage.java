ee0904b1cb61be4f59462a03e56a9524c5a03845
marynagrabchuk <marynagrabchuk@gmail.com>
2023-01-21 21:20:18 -0500

    changed method reverse string

--- src/sessoin1/HomePage.java
+++ src/sessoin1/HomePage.java
@@ -3,10 +3,7 @@ package sessoin1;
 public class HomePage {
     public String reverseString(String word){
         StringBuilder stringBuilder = new StringBuilder(word);
-        String reverseWord = stringBuilder.reverse().toString();
-
-
-        return reverseWord;
+        return stringBuilder.reverse().toString();
     }
     public static void main(String[] args) {
         HomePage homePage  = new HomePage();
