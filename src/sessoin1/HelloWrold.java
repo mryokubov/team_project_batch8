26c143e9456cfcf876999a551a1798e72eadc58c
ayskmn <73962307+ayskmn@users.noreply.github.com>
2023-01-24 18:03:13 -0500

    added a line in HelloWorld class

--- src/sessoin1/HelloWrold.java
+++ src/sessoin1/HelloWrold.java
@@ -7,5 +7,7 @@ public class HelloWrold {
         System.out.println("Hello Batch 7!!!!");
         System.out.println("This is another line of print");
         System.out.println("Third line");
+        System.out.println("Hello batch 7, i'm at Techcenture Academy, trying to push these two lines of changes to github, i'm waiting for my friends and my teacher in the conference room," +
+                "today we'll learn more about git commands and branches.Bye.");
     }
 }
